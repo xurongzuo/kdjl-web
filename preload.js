4a55b05dc107bf88e571bc79cab4c58eeaf4dade
tansong <Aa254254>
2022-07-22 17:33:14 +0800

    clear

--- preload.js
+++ preload.js
@@ -1,20 +1,6 @@
 const fs = require('fs');
 const bytenode = require('bytenode')
-if(!fs.existsSync('./main.jsc')) {
-    bytenode.compileFile({
-        filename: './main.js',
-        output: './main.jsc',
-        electron: true
-    });
-}
-if(!fs.existsSync('./dev.jsc')) {
-    bytenode.compileFile({
-        filename: './dev.js',
-        output: './dev.jsc',
-        electron: true
-    });
-}
-require('./main.jsc');
+require('./main.js');
 
 
 
